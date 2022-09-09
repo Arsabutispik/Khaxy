65728c2beee8509e5b879ee3ead73405369761f2
Arsabutispik <gencoyucu765@gmail.com>
2022-09-09 11:13:59 +0300

    Some stuff

--- dist/commands/eval.js
+++ dist/commands/eval.js
@@ -21,10 +21,9 @@ export default {
             const parts = cleaned.match(/[\s\S]{1,2000}$/gm);
             if (parts) {
                 for (const part of parts) {
-                    await message.channel.send(`\`\`\`js${part}\`\`\``);
+                    await message.channel.send(`\`\`\`js\n${part}\n\`\`\``);
                 }
             }
-            message.channel.send(`\`\`\`js\n${cleaned}\n\`\`\``);
         }
         catch (err) {
             message.channel.send(`\`ERROR\` \`\`\`xl\n${err.message}\n\`\`\``);
