eb226d0ad94f29c060e870a10ead17d53f298d56
Arsabutispik <gencoyucu765@gmail.com>
2023-02-13 15:35:27 +0300

    Voice state

--- dist/events/voiceStateUpdate.js
+++ dist/events/voiceStateUpdate.js
@@ -2,20 +2,37 @@ import { EmbedBuilder } from "discord.js";
 export default async (client, oldState, newState) => {
     if (oldState.channelId === newState.channelId)
         return;
-    if (newState.channelId === null && oldState.channelId !== null && oldState.member?.id === client.user.id) {
-        const player = client.manager.get(oldState.guild.id);
-        if (player) {
-            const textChannel = await oldState.guild.channels.fetch(player.textChannel);
-            if (textChannel) {
-                const leaveEmbed = new EmbedBuilder()
-                    .setAuthor({ name: `Kanaldan Atıldım!`, iconURL: "https://cdn.discordapp.com/attachments/972754239447261264/1074662659737276466/a26bb800-b7ee-11eb-8909-43ff37955c81.png" })
-                    .setTitle("Sesli kanaldan atıldım!")
-                    .setDescription("Sesli kanaldan atıldığımdan dolayı müzik kapatıldı.")
-                    .setColor("Red")
-                    .setTimestamp();
-                await textChannel.send({ embeds: [leaveEmbed] });
+    if (oldState.channelId !== null) {
+        if (oldState.channel?.members.find(user => user.id === client.user.id) && oldState.channel?.members.size === 1) {
+            const player = client.manager.get(oldState.guild.id);
+            if (player) {
+                const textChannel = await oldState.guild.channels.fetch(player.textChannel);
+                if (textChannel) {
+                    const noMember = new EmbedBuilder()
+                        .setTitle("Sesli kanalda kimse kalmadı!")
+                        .setDescription("Sesli kanalda kimse kalmadığı için müzik kapatıldı.")
+                        .setColor("Red")
+                        .setTimestamp();
+                    await textChannel.send({ embeds: [noMember] });
+                }
+                await player.destroy();
             }
-            await player.destroy();
+        }
+        if (newState.channelId === null && oldState.member?.id === client.user.id) {
+            const player = client.manager.get(oldState.guild.id);
+            if (player) {
+                const textChannel = await oldState.guild.channels.fetch(player.textChannel);
+                if (textChannel) {
+                    const leaveEmbed = new EmbedBuilder()
+                        .setAuthor({ name: `Kanaldan Atıldım!`, iconURL: "https://cdn.discordapp.com/attachments/972754239447261264/1074662659737276466/a26bb800-b7ee-11eb-8909-43ff37955c81.png" })
+                        .setTitle("Sesli kanaldan atıldım!")
+                        .setDescription("Sesli kanaldan atıldığımdan dolayı müzik kapatıldı.")
+                        .setColor("Red")
+                        .setTimestamp();
+                    await textChannel.send({ embeds: [leaveEmbed] });
+                }
+                await player.destroy();
+            }
         }
     }
 };
