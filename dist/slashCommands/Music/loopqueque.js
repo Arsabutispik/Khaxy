d6e182e3099e022258a21a1540053bb16f9bb9c4
Arsabutispik <gencoyucu765@gmail.com>
2023-03-02 20:44:56 +0300

    fixes

--- dist/slashCommands/Music/loopqueque.js
+++ dist/slashCommands/Music/loopqueque.js
@@ -15,19 +15,31 @@ export default {
         if (interaction.guild.members.me.voice.channel && interaction.member.voice.channel.id !== interaction.guild.members.me.voice.channel.id) {
             return await interaction.reply("|❌| **Bu komutu kullanmak için aynı ses kanalında olmanız gerekir.**");
         }
-        if (!interaction.member.permissions.has("Administrator")) {
-            if (!interaction.member.roles.cache.has(client.guildsConfig.get(interaction.guild.id).config.djRole)) {
-                await interaction.reply("|❌| **Bu komutu kullanmak için yeterli yetkiniz yok.**");
-                return;
+        if (player.queue.size === 0) {
+            return await interaction.reply("|❌| **Kuyruk boş.**");
+        }
+        const voiceStateUsers = interaction.member.voice.channel.members
+            .filter(member => !member.user.bot)
+            .filter(member => !member.roles.cache.has("798592379204010024"))
+            .filter(member => !member.voice.selfDeaf)
+            .filter(member => !member.voice.serverDeaf)
+            .filter(member => !(member.id === interaction.user.id));
+        if (voiceStateUsers.size > 0) {
+            if (!interaction.member.permissions.has("Administrator")) {
+                if (!interaction.member.roles.cache.has(client.guildsConfig.get(interaction.guild.id).config.djRole)) {
+                    await interaction.reply("|❌| **Bu komutu kullanmak için yeterli yetkiniz yok.**");
+                    return;
+                }
+                else {
+                    const success = player.setQueueRepeat(!player.queueRepeat);
+                    await interaction.reply(`|${success.queueRepeat ? "✅" : "❌"}| **Kuyruk tekrarı ${success.queueRepeat ? "açıldı" : "kapatıldı"}.**`);
+                    return;
+                }
             }
             else {
                 const success = player.setQueueRepeat(!player.queueRepeat);
-                await interaction.reply(`|${success.queueRepeat ? "✅" : "❌"}| **Kuyruk tekrarı ${success.queueRepeat ? "açıldı" : "kapatıldı"}.**`);
-                return;
+                return await interaction.reply(`|${success.queueRepeat ? "✅" : "❌"}| **Kuyruk tekrarı ${success.queueRepeat ? "açıldı" : "kapatıldı"}.**`);
             }
-        }
-        if (player.queue.size === 0) {
-            return await interaction.reply("|❌| **Kuyruk boş.**");
         }
         const success = player.setQueueRepeat(!player.queueRepeat);
         return await interaction.reply(`|${success.queueRepeat ? "✅" : "❌"}| **Kuyruk tekrarı ${success.queueRepeat ? "açıldı" : "kapatıldı"}.**`);
--- dist/slashCommands/Music/shuffle.js
+++ dist/slashCommands/Music/shuffle.js
@@ -14,10 +14,25 @@ export default {
             await interaction.reply("|❌| **Bot şu anda müzik çalmıyor.**");
             return;
         }
-        if (!interaction.member.permissions.has("Administrator")) {
-            if (!interaction.member.roles.cache.has(client.guildsConfig.get(interaction.guild.id).config.djRole)) {
-                await interaction.reply("|❌| **Bu komutu kullanmak için yeterli yetkiniz yok.**");
-                return;
+        const voiceStateUsers = interaction.member.voice.channel.members
+            .filter(member => !member.user.bot)
+            .filter(member => !member.roles.cache.has("798592379204010024"))
+            .filter(member => !member.voice.selfDeaf)
+            .filter(member => !member.voice.serverDeaf)
+            .filter(member => !(member.id === interaction.user.id));
+        if (voiceStateUsers.size > 0) {
+            if (!interaction.member.permissions.has("Administrator")) {
+                if (!interaction.member.roles.cache.has(client.guildsConfig.get(interaction.guild.id).config.djRole)) {
+                    await interaction.reply("|❌| **Bu komutu kullanmak için yeterli yetkiniz yok.**");
+                    return;
+                }
+                else {
+                    player.pause(true);
+                    await interaction.reply("|✅| **Müzik karıştırıldı.**");
+                    const message = await interaction.fetchReply();
+                    await message.react("✅");
+                    return;
+                }
             }
             else {
                 player.pause(true);
