41d7caf24ec352fb4ddf532d565d1f3c6fe334d1
ispik <ispik@outlook.com.tr>
2024-09-22 15:12:17 +0300

    Bug fix

--- src/botconfig.ts
+++ src/botconfig.ts
@@ -21,7 +21,7 @@ export default {
     //Replace these with your own emoji ID's that exists in the application emojis.
     searchEmoji: "1276505335145955421",
     gearSpinning: "1276244551203557428",
-    mailSent: "277019710147264542",
+    mailSent: "1277019710147264542",
     confirm: "1278053289992392795",
     reject: "1278053315334111353",
     ban: "1278053275429634162",
