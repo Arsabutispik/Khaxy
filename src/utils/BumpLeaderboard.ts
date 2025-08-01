ceb104ef5d59a7d103f1c72531727f639357e212
ispik <ispik6441@proton.me>
2025-08-01 13:14:27 +0300

    fix the damn bump leaderboard

--- src/utils/BumpLeaderboard.ts
+++ src/utils/BumpLeaderboard.ts
@@ -41,7 +41,7 @@ export async function bumpLeaderboard(client: Client, guildId: string, lastBump?
       initial += `\n${t("last_bump", { user: lastBump.toString(), time: time(new Date(), "R") })}`;
     }
     if (guild_config.last_bump_winner) {
-      initial += `\n\n${t("last_winner", { user: `<@${guild_config.last_bump_winner}>`, countedBump: guild_config.last_bump_winner_count, totalBumps: guild_config.last_bump_winner_total_count })}`;
+      initial += `\n\n${t("last_winner", { user: `<@${guild_config.last_bump_winner}>`, count: guild_config.last_bump_winner_count || 0, total_bumps: guild_config.last_bump_winner_total_count })}`;
     }
     await message.edit(initial);
   } else {
