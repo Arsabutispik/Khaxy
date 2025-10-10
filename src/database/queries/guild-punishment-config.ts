6199a8bcd4bd6b85bbe779eeb06ba0513b987c86
Arsabutispik <ispik6441@proton.me>
2025-10-10 12:44:40 +0300

    fix: return result from getGuildPunishmentConfig function

--- src/database/queries/guild-punishment-config.ts
+++ src/database/queries/guild-punishment-config.ts
@@ -1,7 +1,7 @@
 import { prisma } from "@database";
 
 export async function getGuildPunishmentConfig(guildId: string, level: number) {
-  await prisma.guild_punishment_config.findUnique({
+  return await prisma.guild_punishment_config.findUnique({
     where: {
       guild_id_level: {
         // Using the composite key
