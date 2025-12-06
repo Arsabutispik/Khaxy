e5452e77edccb59bc574e56ddc9c68ddad5d5cef
Arsabutispik <ispik6441@proton.me>
2025-12-06 17:24:42 +0300

    feat: add .env.example to root and ecosystem.config.js for bot and web app configuration

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: "khaxy-bot",
+      script: "apps/bot/dist/index.js",
+
+      env_file: [".env"],
+
+      env: {
+        NODE_ENV: "production",
+      },
+      exec_mode: "fork",
+      watch: false,
+      ignore_watch: ["node_modules"],
+    },
+  ],
+};
