3396302af707b729f1b4cdf782f33d176bfa2407
Arsabutispik <ispik6441@proton.me>
2025-12-17 13:34:58 +0300

    refactor: rename locale-flags.ts to localeFlags.ts and update exports

--- apps/bot/src/constants/index.ts
+++ apps/bot/src/constants/index.ts
@@ -1,5 +1,2 @@
-export * from "./mod-mail.js";
-export * from "./punishments.js";
-export * from "./infraction.js";
-export * from "./locale-flags.js";
+export * from "./localeFlags.js";
 export * from "./configSchema.js";
--- apps/bot/src/constants/infraction.ts
+++
@@ -1,6 +0,0 @@
-export enum InfractionType {
-  BAN = "ban",
-  KICK = "kick",
-  MUTE = "mute",
-  WARN = "warn",
-}
