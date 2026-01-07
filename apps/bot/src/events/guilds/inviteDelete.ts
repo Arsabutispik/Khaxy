fd820140eda95771e5eb490b8cd80d1d9ac433f9
arsabutispik <ispik6441@proton.me>
2026-01-07 22:28:19 +0300

    refactor(bot): logging improvements for GuildInvite events

--- apps/bot/src/events/guilds/inviteDelete.ts
+++ apps/bot/src/events/guilds/inviteDelete.ts
@@ -1,54 +1,15 @@
 import { EventBase } from "@types";
-import { AuditLogEvent, ChannelType, EmbedBuilder, Events, InviteGuild } from "discord.js";
-import { getGuildConfig } from "src/database/index.js";
-import { returnWebhook, toStringId, WebhookType } from "@utils";
-import { logger } from "@lib";
+import { Events, InviteGuild } from "discord.js";
+import { logInviteDelete } from "@utils";
+import { getOrCreateGuild } from "@repo/database";
 
 export default {
   name: Events.InviteDelete,
   once: false,
   async execute(invite) {
     if (!invite.guild || invite.guild instanceof InviteGuild) return;
-    const guildConfig = await getGuildConfig(invite.guild.id);
+    const guildConfig = await getOrCreateGuild(invite.guild.id);
     if (!guildConfig) return;
-    const t = invite.client.i18next.getFixedT(guildConfig.language, "events", "inviteDelete");
-    if (!guildConfig.invite_logs_channel_id) return;
-    const logChannel = invite.guild.channels.cache.get(toStringId(guildConfig.invite_logs_channel_id));
-    if (logChannel?.type !== ChannelType.GuildText) return;
-
-    const embed = new EmbedBuilder()
-      .setColor("Red")
-      .setTitle(t("embed.title"))
-      .setDescription(
-        t("embed.description", {
-          invite,
-        }),
-      )
-      .setTimestamp();
-    const auditLogs = await invite.guild
-      .fetchAuditLogs({
-        limit: 1,
-        type: AuditLogEvent.InviteCreate,
-      })
-      .catch(() => null);
-    const logEntry = auditLogs?.entries.first();
-    if (logEntry?.target?.code === invite.code) {
-      embed.setFooter({
-        text: logEntry.executor?.username ?? t("unknown_executor"),
-        iconURL: logEntry.executor?.displayAvatarURL() ?? undefined,
-      });
-    }
-    const webhook = await returnWebhook(invite.client, logChannel, invite.guild.id, {
-      id: guildConfig.emoji_logs_webhook_id,
-      type: WebhookType.INVITE_LOGS,
-    });
-    await webhook.send({ embeds: [embed] }).catch((error) => {
-      logger.log({
-        level: "error",
-        error,
-        message: `Failed to send inviteDelete embed in ${invite.guild!.name} (${invite.guild!.id})`,
-        channelId: logChannel.id,
-      });
-    });
+    await logInviteDelete(invite, guildConfig);
   },
 } satisfies EventBase<Events.InviteDelete>;
---
+++ apps/bot/src/utils/logging/guilds/invites/create.ts
@@ -0,0 +1,49 @@
+import { Invite, ChannelType, EmbedBuilder, time, TimestampStyles, InviteGuild } from "discord.js";
+import { GuildWithLogs } from "@repo/database";
+import { returnWebhook, WebhookType } from "@utils";
+import { logger } from "@lib";
+
+export async function logInviteCreate(invite: Invite, guildConfig: GuildWithLogs) {
+  if (!invite.guild || invite.guild instanceof InviteGuild) return;
+  const t = invite.client.i18next.getFixedT(guildConfig.language, "events", "inviteCreate");
+  if (!guildConfig.logConfig?.inviteLogsChannelId) return;
+  const logChannel = invite.guild.channels.cache.get(guildConfig.logConfig.inviteLogsChannelId);
+  if (logChannel?.type !== ChannelType.GuildText) return;
+  const embed = new EmbedBuilder()
+    .setColor("Green")
+    .setTitle(t("embed.title"))
+    .setDescription(
+      t("embed.description", {
+        invite,
+        timestamp:
+          invite.maxAge && invite.maxAge > 0
+            ? time(new Date(Date.now() + invite.maxAge * 1000), TimestampStyles.RelativeTime)
+            : invite.client.allEmojis.get(invite.client.config.emojis.infinity.id)?.format,
+        usage:
+          invite.maxUses && invite.maxUses > 0
+            ? invite.maxUses
+            : invite.client.allEmojis.get(invite.client.config.emojis.infinity.id)?.format,
+      }),
+    )
+    .setTimestamp();
+  if (invite.inviter) {
+    embed.setFooter({
+      text: invite.inviter.username,
+      iconURL: invite.inviter.displayAvatarURL(),
+    });
+  }
+  const webhook = await returnWebhook(invite.client, logChannel, invite.guild.id, guildConfig, {
+    id: guildConfig.logConfig.inviteLogsWebhookId,
+    type: WebhookType.INVITE_LOGS,
+  });
+  if(webhook) {
+    await webhook.send({ embeds: [embed] }).catch((error) => {
+      logger.log({
+        level: "error",
+        error,
+        message: `Failed to send inviteCreate embed in ${invite.guild!.name} (${invite.guild!.id})`,
+        channelId: logChannel.id,
+      });
+    });
+  }
+}
